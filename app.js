2cb779d42ad1c27fd6b88cd977b18198afbcec4f
Rowan <LargeBee@users.noreply.github.com>
2016-12-11 20:03:40 +0000

    Completed app.js
    Finished the app.js (I think)

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var TwitterPackage = require('twitter');
 var MarkovChain = require('markovchain');
 var fs = require('fs');
 var quotes = new MarkovChain(fs.readFileSync('./quotes.txt', 'utf8'));
-var chainSize = 15;
+var chainSize;
+
+var min = 10;
+var max = 20;
+var minutes = 10;
 
 var useUpperCase = function(wordList) {
   var tmpList = Object.keys(wordList).filter(function(word) {
@@ -10,6 +14,20 @@ var useUpperCase = function(wordList) {
   })
   return tmpList[~~(Math.random()*tmpList.length)]
 }
+
+
+the_interval = minutes * 60 * 1000;
+setInterval(function() {
+  console.log("Changing bio...");
+  chainSize = Math.floor(Math.random()*(max-min+1)+min);
+  var quote = quotes.start(useUpperCase).end(chainSize-1).process()
+  Twitter.post('account/update_profile', {description: quote},  function(error){
+    if(error){
+      console.log(error);
+    }
+    console.log("New profile bio: " + quote)
+  });
+}, the_interval);
 
 var secret = {
   consumer_key: 'consumer key',
@@ -22,9 +40,11 @@ var recieveName;
 
 
 Twitter.stream('statuses/filter', {track: '#BerserkMarkov'}, function(stream) {
+  console.log("READY")
   stream.on('data', function(tweet) {
-    console.log("User: @" + tweet.user.screen_name + "\nSent: " + tweet.text);
+    console.log("\nUser: @" + tweet.user.screen_name + "\nSent: " + tweet.text);
     recieveName = "@" + tweet.user.screen_name;
+    chainSize = Math.floor(Math.random()*(max-min+1)+min);
     Twitter.post('statuses/update', {status: recieveName + ' ' + quotes.start(useUpperCase).end(chainSize-1).process()},  function(error, tweet, response){
       if(error){
         console.log(error);
